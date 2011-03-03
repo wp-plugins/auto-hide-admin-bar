b05570f2b53a611a70861533a23e89426ad111de
mbootsman <mbootsman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-03 08:56:59 +0000

    Initial version added -- 0.3
    git-svn-id: https://plugins.svn.wordpress.org/auto-hide-admin-bar/trunk@354475 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ auto-hide-admin-bar.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Plugin Name: Auto Hide Admin Bar
+Plugin URI: http://www.nostromo.nl/plugins/auto-hide-admin-bar
+Description: Automatically hides the admin bar. Will show the admin bar when hovering over the top of the site.
+Author: Marcel Bootsman
+Version: 0.3
+Author URI: http://www.nostromo.nl
+*/
+
+function auto_hide_admin_bar() {
+    wp_enqueue_script('jquery');
+    ?>
+    <script type='text/javascript'>
+
+            jQuery(document).ready(function(){
+
+                jQuery('#wpadminbar').css('top','-28px');
+                jQuery('body').css('margin-top','-28px');
+                jQuery('body').append('<div id=\'hiddendiv\'></div>');
+                
+		$autoHide = jQuery(this).find('#hiddendiv');
+		$autoHide.css('width', '100%');
+		$autoHide.css('min-height', '28px');
+		$autoHide.css('z-index', '99998'); // admin bar is at z-index: 999999;
+		$autoHide.css('position', 'absolute');
+                $autoHide.css('top', '0px');
+
+		$autoHide.mouseenter( function(e) {
+			jQuery('#wpadminbar').animate({'top':'0px'}, 'fast');
+                        jQuery('body').animate({'margin-top':'0px'}, 'fast');
+		});
+
+		jQuery('#wpadminbar').mouseleave( function(e) {
+			jQuery('#wpadminbar').animate({'top':'-28px'}, 'fast');
+                        jQuery('body').animate({'margin-top':'-28px'}, 'fast');
+		});
+        });
+        </script>
+    <?php
+
+}
+function add_my_hide_stuff() {
+    if (is_user_logged_in()) {
+        auto_hide_admin_bar();
+    }
+}
+add_action('wp_head', 'add_my_hide_stuff');
+?>
