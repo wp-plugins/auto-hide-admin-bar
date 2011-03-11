50fcf989d085145cd726e8eb47586d60e77da3ab
mbootsman <mbootsman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-11 16:22:03 +0000

    tagging version 0.6.1
    git-svn-id: https://plugins.svn.wordpress.org/auto-hide-admin-bar/tags/0.6.1@358831 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- auto-hide-admin-bar.php
+++ auto-hide-admin-bar.php
@@ -1,12 +1,14 @@
 <?php
 /*
 Plugin Name: Auto Hide Admin Bar
-Plugin URI: http://www.nostromo.nl/plugins/auto-hide-admin-bar
+Plugin URI: http://www.nostromo.nl/wordpress-plugins/auto-hide-admin-bar
 Description: Automatically hides the admin bar. Will show the admin bar when hovering over the top of the site.
 Author: Marcel Bootsman
-Version: 0.4
+Version: 0.6.1
 Author URI: http://www.nostromo.nl
 */
+
+
 
 function auto_hide_admin_bar() {
 
@@ -26,28 +28,54 @@ function auto_hide_admin_bar() {
 		$autoHide.css('position', 'fixed');
                 $autoHide.css('top', '0px');
 
-		$autoHide.mouseenter( function(e) {
-			jQuery('#wpadminbar').animate({'top':'0px'}, 'fast');
-                        jQuery('body').animate({'margin-top':'0px'}, 'fast');
-		});
+                var configIn = {
+                    over:adminBarIn, // function = onMouseOver callback (REQUIRED)
+                    sensitivity: 6,
+                    out: doNothing // function = onMouseOut callback (REQUIRED)
+                };
+                var configOut = {
+                    over: doNothing, // function = onMouseOver callback (REQUIRED)
+                    timeout: 1500, // number = milliseconds delay before onMouseOut
+                    out: adminBarOut // function = onMouseOut callback (REQUIRED)
+                };
 
-		jQuery('#wpadminbar').mouseleave( function(e) {
-			jQuery('#wpadminbar').animate({'top':'-28px'}, 'fast');
-                        jQuery('body').animate({'margin-top':'-28px'}, 'fast');
-		});
-        });
+                $autoHide.hoverIntent(configIn);
+                jQuery('#wpadminbar').hoverIntent(configOut);
+
+                // doNothing function is for enabling hoverIntent to work with two layers.
+                function doNothing(){}
+
+                // Show the Admin Bar
+                function adminBarIn() {
+                    jQuery('#wpadminbar').animate({'top':'0px'}, 'fast');
+                    jQuery('body').animate({'margin-top':'0px'}, 'fast');
+                }
+                // Hide the Admin Bar
+                function adminBarOut() {
+                    jQuery('#wpadminbar').animate({'top':'-28px'}, 'fast');
+                    jQuery('body').animate({'margin-top':'-28px'}, 'fast');
+                }
+            });
         </script>
-    <?php
-
+<?php
 }
+
 function add_jquery_stuff() {
     if (is_user_logged_in()) {
-       wp_enqueue_script('jquery');
+        /* determine plugin path */
+        $x = WP_PLUGIN_URL.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__));
+
+        wp_enqueue_script('jquery');
+        wp_register_script( 'jquery-hoverintent', $x.'js/jquery.hoverIntent.minified.js');
+        wp_enqueue_script( 'jquery-hoverintent' );
+        
     }
 }
 add_action('wp_print_scripts','add_jquery_stuff');
 
 function add_my_hide_stuff() {
+
+
     if (is_user_logged_in()) {
         auto_hide_admin_bar();
     }
